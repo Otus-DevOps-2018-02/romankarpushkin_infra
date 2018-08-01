bf4c79a3bfeb06fbf414b0335de03860498b2d10
Roman Karpushkin <karpushkin.roman@yandex.ru>
2018-08-01 13:34:58 +0300

    Ansible: roles, environments and best practices

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -12,13 +12,15 @@ resource "google_compute_instance" "db" {
 
   network_interface {
     network       = "default"
-    access_config = {}
+   access_config = {}
   }
+
 
   metadata {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
 }
+
 
 resource "google_compute_firewall" "firewall_mongo" {
   name    = "allow-mongo-default"
